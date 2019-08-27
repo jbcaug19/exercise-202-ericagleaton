a10d7b67f9657bee35ed4169ed21f7627e41406b
ericagleaton <ericagleaton@comcast.net>
2019-08-27 15:23:36 -0400

    2.02 Creating A Java Bean- Student

---
+++ src/main/java/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+    private String name;
+    private int age;
+    private float height;
+    private String phone_number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+}
